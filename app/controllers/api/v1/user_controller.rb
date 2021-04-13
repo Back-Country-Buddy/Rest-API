6e28f7bc7d593c9c4dafcfc1a2ba0c73df466106
awosborn <alexander.w.osborne@gmail.com>
2021-04-13 15:24:39 -0600

    Add find user endpoint, happy and sad path test

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -6,4 +6,12 @@ class Api::V1::UserController < ApplicationController
       render json: {"error" => {}}, status:404
     end
   end
+
+  def show
+    begin
+      render json: UserSerializer.new(User.find(params[:id]))
+    rescue
+      render json: {"error" => {}}, status:404
+    end
+  end
 end
