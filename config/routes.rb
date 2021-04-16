7615bde8b6194cbfa44e9fa6e4bd7277d0ab9ad0
awosborn <alexander.w.osborne@gmail.com>
2021-04-15 19:22:24 -0600

    well do it live

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      resources :user do
-        resources :tour do
-          resources :plan
-        end
-      end
+    namespace :private do
+      namespace :v1 do
+        resources :user do
+          resources :tour do
+            resources :plan
+         end
+       end
       resources :tour
       resources :plan
+      end
     end
   end
 end
