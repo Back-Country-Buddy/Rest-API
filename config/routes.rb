dde69be5ada78c7a06d9e1e70bbea82fe4eff090
awosborn <alexander.w.osborne@gmail.com>
2021-04-17 12:08:11 -0600

    Add preflight_check to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  match '*all', controller: 'application', action: 'cors_preflight_check', via: [:options]
+
+  
   namespace :api do
     namespace :private do
       namespace :v1 do
