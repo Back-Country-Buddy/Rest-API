8ef300e4a2359abacc407b1d044cb0b307c483b5
awosborn <alexander.w.osborne@gmail.com>
2021-04-19 11:19:08 -0600

    Add departure check to Tours table

--- db/migrate/20210413190632_create_plans.rb
+++ db/migrate/20210413190632_create_plans.rb
@@ -8,6 +8,7 @@ class CreatePlans < ActiveRecord::Migration[5.2]
       t.string :route_alternative, :default => "nil"
       t.string :emergency_plan, :default => "nil"
       t.boolean :checklist, :default => false
+      t.boolean :departure_check, :default => false
       t.string :debrief_conditions, :default => "nil"
       t.string :debrief_decisions, :default => "nil"
       t.string :debrief_plan, :default => "nil"
