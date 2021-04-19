1db3aab09612d8e77c9ad5e19594769f9a355ae1
Liam Cusack <LCusack8108@gmail.com>
2021-04-19 09:58:58 -0600

    update dependant destroy

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,8 +2,8 @@ class Tour < ApplicationRecord
   validates_presence_of :date,
                         :location,
                         :creator_id
-                        
-  has_many :tour_users
+
+  has_many :tour_users, dependent: :destroy
   has_many :plans, dependent: :destroy
   has_many :users, through: :tour_users
 end
